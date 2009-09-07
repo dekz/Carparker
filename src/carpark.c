36c450edee39cffd3ec6944f4f127598db03674f
Bodaniel Jeanes <me@bjeanes.com>
2009-09-07 23:04:18 +1000

    Epic fucking around

--- src/carpark.c
+++ src/carpark.c
@@ -30,14 +30,13 @@
 #define FALSE 0
 #define TRUE !FALSE
 
-
 /* An example of the data structure of the Car Park. You may define your own car park */
 
 typedef struct {
     char *buffer[CAR_PARK_SIZE];       // stores carpark cars
     char *arrival_time[CAR_PARK_SIZE]; // stores arrival time of cars
-    int  keep_running;		         // set false to exit threads
-    int  size;			         // current size of carpark
+    int  keep_running;                 // set false to exit threads
+    int  size;                     // current size of carpark
 } CarPark;
 
 
@@ -52,44 +51,133 @@ void remove_car();
 void show_cars();
 char *new_car();
 int  thread_sleep(int time_out_ms);
+int  thread_sleep_default();
 char *time_stamp();
 char get_key();
 void exit_with_error(char *message);
-int is_parking_available();
+int is_carpark_full();
+int is_carpark_empty();
+void start_threads();
+void join_threads();
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t t_arrival_queue;
+pthread_t t_enter_carpark;
+pthread_t t_departure;
+pthread_t t_monitor;
 
 int main() {
-	printf("Welcome to %s!\n", CAR_PARK);
-	enter_carpark(NULL);
-	exit(0);
+    printf("Welcome to %s!\n", CAR_PARK);
+    start_threads();
+    
+    join_threads();
+    exit(0);
+}
+
+void start_threads() {
+    pthread_mutex_init(&mutex, NULL);
+    
+    pthread_create(&t_arrival_queue, NULL, arrival_queue, NULL);
+    pthread_create(&t_enter_carpark, NULL, enter_carpark, NULL);
+    pthread_create(&t_departure,     NULL, departure,     NULL);
+    pthread_create(&t_monitor,       NULL, monitor,       NULL);
+}
+
+void join_threads() {
+    pthread_join(t_arrival_queue, NULL);
+    pthread_join(t_enter_carpark, NULL);
+    pthread_join(t_departure,     NULL);
+    pthread_join(t_monitor,       NULL);
+
+    pthread_mutex_destroy(&mutex);
+}
+
+void *monitor(void *arg) {
+    char c;
+    
+    while( c = getchar() ) {
+		if(c=='Q'||c=='q') {
+            printf("Quitting...\n");
+            
+            pthread_cancel(t_enter_carpark);
+            pthread_cancel(t_departure);
+            pthread_cancel(t_arrival_queue);
+            
+            break;
+        } else if(c=='C'||c=='c') {
+            printf("Printing summary...\n");
+        } else {
+            printf("Invalid key. Use either Q or C\n");
+        }
+	}
+    
+    return NULL;
 }
 
 void *enter_carpark(void *arg) {
-	while(TRUE) {
-		if(!is_parking_available()) {
-			printf("No parking bays available. Arrival blocked\n");
-			thread_sleep(500);
-		}
-	}
-	return NULL;
+    
+    puts("I am enter_carpark");
+    // while(TRUE) {
+    //     if(is_carpark_full()) {
+    //         printf("No parking bays available. Arrival blocked\n");
+    //     } else {
+    //     }
+    //     
+    //     thread_sleep(500);
+    // }
+    return NULL;
 }
 
-int is_parking_available() {
-	return 0;
+void *departure(void *arg) {
+    thread_sleep_default();
+    
+    puts("I am departure");
+    
+    // while(TRUE) {
+    //     if(is_carpark_empty()) {
+    //         printf("Car park empty.  Departure blocked");
+    //     } else {
+    //     }
+    //     
+    //     thread_sleep(500);
+    // }
+    return NULL;
+}
+
+void *arrival_queue(void *arg) {
+    puts("I am arrival_queue");
+    int i = 1;
+
+    while(i++) {
+        printf("%d\n", i);
+        thread_sleep_default();
+    }
+    
+    // puts("I am no longer arrival_queue");
+    return NULL;
+}
+
+
+int is_carpark_full() {
+    return FALSE;
+}
+
+int is_carpark_empty() {
+    return FALSE;
 }
 
 void exit_with_error(char *message) {
-	fprintf(stderr, "%s", message);
-	exit(1);
+    fprintf(stderr, "%s", message);
+    exit(1);
 }
 
 int thread_sleep(int ms) {
-	struct timespec t;
-	t.tv_sec = 0;
-	t.tv_nsec = ms * 1000000;
-	return nanosleep(&t, NULL);
+    struct timespec t;
+    t.tv_sec = 0;
+    t.tv_nsec = ms * 1000000;
+    return nanosleep(&t, NULL);
 }
 
-
-
+int thread_sleep_default() {
+    return thread_sleep(TIME_OUT_SLEEP);
+}
