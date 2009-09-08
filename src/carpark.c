1bc73ad7a015e79a25cc3630548e837535800b22
Bodaniel Jeanes <me@bjeanes.com>
2009-09-08 23:06:23 +1000

    Add some default text from the docs

--- src/carpark.c
+++ src/carpark.c
@@ -1,9 +1,14 @@
 #include "carpark.h"
 
 int main() {
-    printf("Welcome to %s!\n", CAR_PARK);
     signal(SIGINT, quit);
     srand(time(0));
+    
+    printf("Welcome to the simulation of a Car Park Management System for %s.\n\n", CAR_PARK);
+    puts("Press 'C/c' (followed by return) to show car park status.");
+    puts("Press 'Q/q' (followed by return) to quit program.");
+    puts("\n<--- Press return to continue --->\n");
+    getchar();
 
     puts_example_car_id();
         
