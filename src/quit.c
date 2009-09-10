8b4486a426f5bad0ef9683c4a41fd1c84fe7cff0
Bodaniel Jeanes <me@bjeanes.com>
2009-09-11 00:20:25 +1000

    Forgot quit.h

---
+++ src/quit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "helpers.h"
+#include "quit.h"
+
+void quit() {
+    puts("Quitting...");
+    signal(SIGINT, force_quit);
+    stop_running();
+}
+
+void force_quit() {
+    exit_with_error("Hard shutdown...");
+}
---
+++ src/quit.h
@@ -0,0 +1,4 @@
+#include "helpers.h"
+
+void quit();
+void force_quit();
