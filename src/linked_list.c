b62fe1baecd9eb71cda839a733878de84ca058d5
Jacob Evans <dekz@dekz.net>
2009-09-11 11:53:07 +1000

    Fixed committed merge conflict

--- src/linked_list.c
+++ src/linked_list.c
@@ -45,81 +45,33 @@ node *new_node(void *arg)
 
 void delete_node(int n)
 {
-	printf("DELETE NODE START\n");
+
 	lock();
     node *p = _ll.head;
-<<<<<<< HEAD:src/linked_list.c
-    if (_ll.size == 0)
-    {
-        printf("Error: No Cars to Remove\n");
-    } else if (n == _ll.size)
-    {
-        print_car_departure(p->car, n);
-        int i = 1;
-        for (i=1; i < _ll.size-1; i++)
-        {
-            p = p->next;
-        }
-        node *d = p->next;
-        p->next = NULL;
-        _ll.size--;
-        free(d);
-    } else if (n == 0)
-    {
-        //remove first
-        node *d = p;
-        _ll.head = p->next;
-        _ll.size--;
-        free(d);
-    }
-    else
-    {
-        int i=1;
-        for (i=1; i < _ll.size-1; i++)
-        {
-            p = p->next;
-        }
-        
-        //p should be the 2nd last node
-        node *d = p->next;
-        //d is now the last node
-        p->next = NULL;
-        print_car_departure(d->car, n);
-        free(d);
-        _ll.size--;
-    }
-=======
     
 	if (n == 1)
 	{
 		//remove first node
-		printf("REMOVING FIRST NODE\n");
 		_ll.head = NULL;
 		_ll.size--;
-		printf("[D] Car Departing -> %s\n", get_car_id(p->car));
+        print_car_departure(p->car, n);
 		free(p);
 	} else 
 	{
-		int i=1;
+		int i=0;
 		//return the 2nd last node
-		for(i=1; i < n-1; i++)
+		for(i=0; i < _ll.size-1; i++)
 		{
 			p = p->next;
 		}
-		printf("CRASH ZONE 1\n");
 		node *d = p->next;
-		printf("CRASH ZONE 2\n");
 		p->next = NULL;
 		_ll.size--;
-		printf("CRASH ZONE 3\n");
-		printf("[D] Car Departing -> %s\n", get_car_id(d->car));
+        print_car_departure(d->car, n);
 		free(d);
-		printf("CRASH ZONE 4\n");
 		
 	}
-	printf("DELETE NODE END\n");
-
->>>>>>> rewrote delete_node:src/linked_list.c
+	
     unlock();
     
 }
